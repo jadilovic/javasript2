19c8490ad707188f099c982998596d7f16d23c6d
jadilovic <adilovic79@yahoo.com>
2020-08-05 13:54:21 +0200

    new commit

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,6 +1,45 @@
+let name = prompt("What is your name?", "");
+alert(`your name is ${name}`);
+
+let isBoss = confirm("Are you the boss?");
+alert(isBoss);
+
+let value = "#value";
+
+value = "Hello World";
+
+let result = prompt("how old are you");
+
+alert(`you are old ${result} years`);
+
 /**
  * http://usejsdoc.org/
- */
+
 
 alert("Whaaaat");
 [1, 2].forEach(alert);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+alert(1 / 0);
+alert("not a number" / 8);
+	 */	
+
+console.log(typeof true);
+alert(typeof "ha");
+
+		let str = "Hello";
+		let str2 = 'Single quotes are ok too';
+		let phrase = `can embed another ${str} ${str2}`;
+		
+		//alert(phrase);
+		
+		let isGreater = 4 > 1;
+
+		//alert( isGreater ); // true (the comparison result is "yes")
