df7e013ffeb05a1378e95540117e62339db4be80
jadilovic <adilovic79@yahoo.com>
2020-08-09 13:23:20 +0200

    functions

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,4 +1,21 @@
+function checkAge(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return confirm('Do you have permission from your parents?');
+  }
+}
 
+let age = prompt('How old are you?', 18);
+
+if ( checkAge(age) ) {
+  alert( 'Access granted' );
+} else {
+  alert( 'Access denied' );
+}
+
+
+/**
 
 function showMessage2(from, hello) {
  
