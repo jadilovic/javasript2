6d7d0d7f4bc1b89d3dc5041e3e89fed6fb0dc522
jadilovic <adilovic79@yahoo.com>
2020-08-12 12:13:51 +0200

    functions 4

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,3 +1,114 @@
+for (let i = 0; i < 5; i++) {
+  console.log("value,", i);
+}
+
+let value = prompt("ime", "napisi");
+
+alert(value);
+
+let answer = confirm("good or bad");
+
+alert(answer);
+
+/**
+
+let ask = (question, yes, no) => (confirm(question)) ? yes() : no();
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+
+/**
+
+let talk = () => {
+	return "Saying";
+}
+
+alert(talk());
+
+let sum = (a, b) => {
+	let result = a + b;
+	return result;
+}
+
+alert(sum(5, 7));
+
+/**
+
+let age = prompt("koliko godina", 18);
+
+let covjek = (age < 18) ?
+		() => alert("Momak") :
+			() => alert("Muskarac");
+			
+			covjek();
+
+/**
+let age = prompt("What is your age?", 18);
+
+let dobroDosli = (age < 18) ? 
+		function(){alert("Boy")} :
+			function(){alert("Man")};
+
+	dobroDosli();		
+			
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Hello!");
+  };
+
+} else {
+
+  welcome = function() {
+    alert("Greetings!");
+  };
+
+}
+
+welcome(); // ok now
+
+
+/**
+
+sayHi("Aki");
+//pozdrav("Bobi");
+
+function sayHi(name){
+	alert(`pozdravi ${name}`);
+}
+
+let pozdrav = function(name){
+	alert(`hello ${name}`);
+}
+
+pozdrav("Cuni");
+
+/**
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+function showOk() {
+  alert( "You agreed." );
+}
+
+function showCancel() {
+  alert( "You canceled the execution." );
+}
+
+// usage: functions showOk, showCancel are passed as arguments to ask
+ask("Do you agree?", showOk, showCancel);
+
+ask("What the fuck?", showOk, showCancel);
+
+
+/**
 let x = prompt("Broj x", "");
 let n = prompt("Broj n", "");
 
