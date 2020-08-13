51810943f73d53b924ef80bca4180fa80a853eed
jadilovic <adilovic79@yahoo.com>
2020-08-13 21:28:57 +0200

    test 2

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -5,5 +5,22 @@ describe("pow", function() {
   it("raises to n-th power", function() {
     assert.equal(pow(2, 3), 8);
   });
+  
+  it("3 raised to the power of 4", function() {
+	    assert.equal(pow(3, 4), 81);
+	  });
+  
+  function makeTest(x){
+	  let expected = x * x * x;
+	  it(`${x} raised to the power of 3 is ${expected}`, 
+		function(){
+		  	assert.equal(pow(x, 3), expected);
+	  		}
+	  	);
+  	}
+  
+  for(let i = 1; i < 5; i++){
+	  makeTest(i);
+  }
 
 });
