7afe1dbc914c5f68e787a99292c8c46df9a7f9e7
jadilovic <adilovic79@yahoo.com>
2020-08-17 09:34:27 +0200

    objects k

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,4 +1,138 @@
+let user = {
+  name: "John",
+  sizes: {
+    height: 182,
+    width: 50
+  }
+};
 
+let clone = Object.assign({}, user);
+
+alert(user === clone);
+
+alert( user.sizes === clone.sizes ); // true, same object
+
+// user and clone share sizes
+user.sizes.width++;       // change a property from one place
+alert(clone.sizes.width); // 51, see the result from the other one
+
+let deep = _.cloneDeep(user);
+console.log(deep[0] === objects[0]);
+// => false
+
+/**
+let user = {
+  name: "John",
+  sizes: {
+    height: 182,
+    width: 50
+  }
+};
+
+alert( user.sizes.height ); // 182
+
+/**
+let user = {name: "Jasmin", age: 44};
+
+let admin = Object.assign({}, user);
+
+for(key in admin)
+	alert(admin[key]);
+
+alert(user == admin);
+
+
+/**
+let user = { name: "John" };
+
+let permissions1 = { canView: true };
+let permissions2 = { canEdit: true };
+
+// copies all properties from permissions1 and permissions2 into user
+Object.assign(user, permissions1, permissions2);
+
+
+let admin = {name: "Master"};
+
+Object.assign(admin, user);
+
+
+for(key in admin)
+alert(admin[key]);
+
+// now user = { name: "John", canView: true, canEdit: true }
+
+/**
+
+let user = {
+  name: "John",
+  age: 30
+};
+
+let clone = {}; // the new empty object
+
+// let's copy all user properties into it
+for (let key in user) {
+	alert(key + ":, " + user[key]);
+  clone[key] = user[key];
+}
+
+// now clone is a fully independent object with the same content
+clone.name = "Pete"; // changed the data in it
+
+alert( user.name ); // still John in the original object
+
+/**
+let user = {name: "John"};
+
+let admin = user;
+
+admin.name = "Bob";
+
+alert(user.name);
+
+
+/**
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+for(key in menu){
+	alert(menu[key]);
+}
+
+function multiplyNumeric(menu){
+	for(key in menu){
+		if(typeof menu[key] == "number")
+		menu[key] *= 2;
+	}
+}
+
+/**
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = calculate(salaries);
+alert(sum);
+
+function calculate(salaries){
+	let sum = 0;
+	for(salary in salaries){
+		sum += salaries[salary];
+	}
+	return sum;
+}
+
+/**
 let schedule = {};
 
 alert(isEmpty(schedule));
