5d3efe58919e777895c8260ca72695424cdb4143
jadilovic <adilovic79@yahoo.com>
2020-09-26 09:24:52 +0200

    loops

--- src/main/resources/static/js/tenary.js
+++ src/main/resources/static/js/tenary.js
@@ -9,6 +9,19 @@ function update(bgColor, textColor){
 	html.style.color = textColor;
 }
 
-select.onchange = function(){
+choice.onchange = function(){
 	(choice.value === "black") ? update("black", "white") : update("white", "black");
 }
+
+
+// CAT LOOP EXAMPLE
+
+const cats = ["cica", "mica", "maca"];
+const par = document.querySelector(".cat");
+let myCats = "Moje macke se zovu: ";
+
+for(let i = 0; i < cats.length; i++){
+	myCats += cats[i] + ", "
+}
+
+par.textContent = myCats;
