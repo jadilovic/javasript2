5280650a5bb7e6f203309a88f01189ddce6292ec
jadilovic <adilovic79@yahoo.com>
2020-09-27 10:02:13 +0200

    loops continued

--- src/main/resources/static/js/tenary.js
+++ src/main/resources/static/js/tenary.js
@@ -25,3 +25,112 @@ for(let i = 0; i < cats.length; i++){
 }
 
 par.textContent = myCats;
+
+
+// SEARCH LOOP EXAMPLE
+
+const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const para = document.querySelector(".look");
+
+btn.addEventListener("click", function(){
+	let searchName = input.value.toLowerCase();
+	input.value = "";
+	input.focus();
+	for(let i = 0; i < contacts.length; i++){
+		let name = contacts[i].split(":");
+		if(name[0].toLowerCase() === searchName){
+			para.textContent = name[0] + "\'s number is: " + name[1];
+			break;
+		} else {
+			para.textContent = "Contact was not found.";
+		}
+	}
+});
+
+
+// SAMPLE GENERATING INTEGER SQUARES
+
+const enter = document.querySelector(".cal");
+const btnx = document.querySelector(".btn");
+const parax = document.querySelector(".sqr");
+
+btnx.addEventListener("click", function(){
+	let num = enter.value;
+	
+	for(let i = 1; i <= num; i++){
+		let sqrr = Math.sqrt(i);
+		if(Math.floor(sqrr) !== sqrr){
+			continue;
+		}
+		parax.textContent += i + ", ";
+	}
+});
+
+
+// SAMPLE LAUNCH COUNTDOWN
+
+const output = document.querySelector('.output');
+
+let i = 10;
+
+while(i >= 0){
+	const para = document.createElement('p');
+	if(i === 10)
+		para.textContent = "Countdown " + i;
+	else if(i === 0)
+		para.textContent = "Blast off";
+	else
+		para.textContent = i;
+	output.appendChild(para);
+	i--;
+}
+
+
+// SAMPLE TWO LISTS OF NAMES
+
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector(".adm");
+const refused = document.querySelector(".ref");
+
+admitted.textContent = "Admit: ";
+refused.textContent = "Refuse: ";
+let count = 0;
+
+for(let i = 0; i < people.length; i++){
+	if(people[i] === "Phil" || people[i] === "Lola"){
+		count++;
+		refused.textContent += (count === 2) ? people[i] + "." : people[i] + ", ";
+	} else {
+		admitted.textContent += (i === (people.length - 1)) ? people[i] + "." : people[i] + ", ";
+	}
+}
+	
+	
+// SAMPLE LOOP LIST
+
+let myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
+let list = document.createElement('ul');
+
+// Add your code here
+
+for(let i = 0; i < myArray.length; i++){
+	let item = document.createElement("li");
+	item.textContent = myArray[i];
+	list.append(item);
+}
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+section.appendChild(list);
+	
+	
+	
+	
+
+
+
