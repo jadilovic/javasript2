44f4444178591656cdd4af0fcb6ac1ca7c86de08
jadilovic <adilovic79@yahoo.com>
2020-09-27 13:51:22 +0200

    loops continued 2

--- src/main/resources/static/js/tenary.js
+++ src/main/resources/static/js/tenary.js
@@ -125,10 +125,39 @@ for(let i = 0; i < myArray.length; i++){
 
 // Don't edit the code below here!
 
-const section = document.querySelector('section');
+const section = document.querySelector('.preview');
 section.appendChild(list);
 	
+
+// SECOND SEARCH SAMPLE WITH LOOP
 	
+let name = 'Mustafa';
+let j = 0;
+let parag = document.createElement('p');
+
+let phonebook = [
+  { name : 'Chris', number : '1549' },
+  { name : 'Li Kang', number : '9634' },
+  { name : 'Anne', number : '9065' },
+  { name : 'Francesca', number : '3001' },
+  { name : 'Mustafa', number : '6888' },
+  { name : 'Tina', number : '4312' },
+  { name : 'Bert', number : '7780' },
+  { name : 'Jada', number : '2282' },
+]
+
+// Add your code here
+while(j < phonebook.length){
+	if(phonebook[j].name === name){
+		parag.textContent = phonebook[j].name + ", phone num: " + phonebook[j].number;
+		break;
+	}
+	j++;
+}
+
+// Don't edit the code below here!
+let sectionx = document.querySelector('.book');
+sectionx.appendChild(parag);
 	
 	
 
