21071a34b4f3c207191371fd46f177657c362473
jadilovic <adilovic79@yahoo.com>
2020-10-02 09:56:00 +0200

    what

--- src/main/resources/static/js/valid.js
+++ src/main/resources/static/js/valid.js
@@ -1,3 +1,4 @@
+/**
 const form = document.querySelector("form");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
@@ -9,7 +10,7 @@ form.onsubmit = function(e){
 		para.textContent = "You need to fill in both names";
 	}
 }
-
+**/
 
 const btn = document.querySelector('button');
 const videoBox = document.querySelector('div');
@@ -23,6 +24,36 @@ videoBox.onclick = function() {
   videoBox.setAttribute('class','hidden');
 };
 
-video.onclick = function() {
+video.onclick = function(e) {
+	e.stopPropagation();
   video.play();
 };
+
+
+//Get the element, add a click listener...
+document.getElementById("parent-list").addEventListener("click", function(e) {
+	// e.target is the clicked element!
+	// If it was a list item
+	if(e.target && e.target.nodeName == "LI") {
+		// List item found!  Output the ID!
+		console.log("List item ", e.target.id.replace("post-", ""), " was clicked!");
+	}
+});
+
+
+
+//Get the parent DIV, add click listener...
+document.getElementById("myDiv").addEventListener("click",function(e) {
+	// e.target was the clicked element
+  if (e.target && e.target.matches("a.classA")) {
+    console.log("Anchor element clicked!");
+	}
+});
+
+
+
+
+
+
+
+
