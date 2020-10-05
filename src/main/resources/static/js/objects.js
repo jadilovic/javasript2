dae66fe42871e46352794d3db2a943833184bfb6
jadilovic <adilovic79@yahoo.com>
2020-10-05 18:42:04 +0200

    objects 3

--- src/main/resources/static/js/objects.js
+++ src/main/resources/static/js/objects.js
@@ -13,6 +13,48 @@ function Shape(name, sides, sideLength) {
   	    let square = new Shape("square", 4, 5);
   	    square.calcPerimeter();
   	    
+  	    let square2 = Object.create(square);
+  	    
+  	    let square3 = new square2.constructor("kocka", 2, 3);
   	    
   	    
+  	    // CREATING OBJECTS 
+  	    
+    	  function Person(first, last, age, gender, interests) {
+    		  this.name = {
+    				  first: first,
+    				  last: last
+    		  		},
+    		  this.age = age,
+    		  this.gender = gender,
+    		  this.interests = interests,
+    		  this.bio = function(){
+    			  let heshe = (this.gender === "male") ? "he" : "she";
+    			  let interestsList = "";
+    			  for(let i = 0; i < interests.length; i++){
+    				  if(i === (interests.length - 2)){
+    	  				  interestsList += interests[i] + " and ";
+    				  } else if (i === (interests.length - 1)){
+    	  				  interestsList += interests[i] + ".";
+    				  } else {
+    	  				  interestsList += interests[i] + ", ";
+    				  }
+    			  }
+    			  alert(this.name.first + " is " + this.age + " years old and " + heshe + " has interests " + interestsList);
+    		  }
+    		  this.greeting = function() {
+    		    alert('Hi! I\'m ' + this.name.first + '.');
+    		  }
+    		}
+    	  
+    	  Person.prototype.farewell = function(){
+    		  alert(this.name.first + " has left the building");
+    	  }
+  	    
+    	  let person = new Person("Jasmin", "Adilovic", 42, "male", ["golf", "skiing", "singing"]);
+    	  
+    	  
+    	  
+    	  
+    	  
   	    
