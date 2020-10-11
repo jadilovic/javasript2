b319f7863b18f937dc24cd57ed3b1bd2808e9b87
jadilovic <adilovic79@yahoo.com>
2020-10-11 13:16:17 +0200

    json 22

--- src/main/resources/static/js/jsontest.js
+++ src/main/resources/static/js/jsontest.js
@@ -1,48 +1,118 @@
     const header = document.querySelector('header');
-    const section = document.querySelector('section');
+    const sectionH = document.querySelector('.heroes');
 
+    let request = new XMLHttpRequest();
+    
+    let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 
+    request.open('GET', requestURL);
 
-let superHeroes = {
-  "squadName": "Super hero squad",
-  "homeTown": "Metro City",
-  "formed": 2016,
-  "secretBase": "Super tower",
-  "active": true,
-  "members": [
-    {
-      "name": "Molecule Man",
-      "age": 29,
-      "secretIdentity": "Dan Jukes",
-      "powers": [
-        "Radiation resistance",
-        "Turning tiny",
-        "Radiation blast"
-      ]
-    },
-    {
-      "name": "Madame Uppercut",
-      "age": 39,
-      "secretIdentity": "Jane Wilson",
-      "powers": [
-        "Million tonne punch",
-        "Damage resistance",
-        "Superhuman reflexes"
-      ]
-    },
-    {
-      "name": "Eternal Flame",
-      "age": 1000000,
-      "secretIdentity": "Unknown",
-      "powers": [
-        "Immortality",
-        "Heat Immunity",
-        "Inferno",
-        "Teleportation",
-        "Interdimensional travel"
-      ]
-    }
-  ]
-}
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+    	  const superHeroes = request.response;
+    	  populateHeader(superHeroes);
+    	  showHeroes(superHeroes);
+    	}
     	  
+    function populateHeader(jsonObj) {
+    	  const myH1 = document.createElement('h1');
+    	  myH1.textContent = jsonObj['squadName'];
+    	  header.appendChild(myH1);
+
+    	  const myPara = document.createElement('p');
+    	  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    	  header.appendChild(myPara);
+    	}
+
+    
+    function showHeroes(jsonObj) {
+    	  const heroes = jsonObj['members'];
+    	      
+    	  for (let i = 0; i < heroes.length; i++) {
+    	    const myArticle = document.createElement('article');
+    	    const myH2 = document.createElement('h2');
+    	    const myPara1 = document.createElement('p');
+    	    const myPara2 = document.createElement('p');
+    	    const myPara3 = document.createElement('p');
+    	    const myList = document.createElement('ul');
+
+    	    myH2.textContent = heroes[i].name;
+    	    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+    	    myPara2.textContent = 'Age: ' + heroes[i].age;
+    	    myPara3.textContent = 'Superpowers:';
+    	        
+    	    const superPowers = heroes[i].powers;
+    	    for (let j = 0; j < superPowers.length; j++) {
+    	      const listItem = document.createElement('li');
+    	      listItem.textContent = superPowers[j];
+    	      myList.appendChild(listItem);
+    	    }
+
+    	    myArticle.appendChild(myH2);
+    	    myArticle.appendChild(myPara1);
+    	    myArticle.appendChild(myPara2);
+    	    myArticle.appendChild(myPara3);
+    	    myArticle.appendChild(myList);
+
+    	    sectionH.appendChild(myArticle);
+    	  }
+    	}
+    
+    
+    // CATS TEST YOUR SKILLS
+    
+    const sectionC = document.querySelector('.cats');
+
+    let para1 = document.createElement('p');
+    let para2 = document.createElement('p');
+
+    let motherInfo = 'The mother cats are called ';
+    let kittenInfo;
+
+    fetch('sample.json')
+    .then(response => response.text())
+    .then(text => displayCatInfo(text))
+
+    function displayCatInfo(catString) {
+      let total = 0;
+      let male = 0;
+
+      // Add your code here
+
+
+
+    // Don't edit the code below here!
+
+      para1.textContent = motherInfo;
+      para2.textContent = kittenInfo;
+    }
+
+    sectionC.appendChild(para1);
+    sectionC.appendChild(para2);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   	    
