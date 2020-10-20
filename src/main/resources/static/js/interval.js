fbbafa1d3ff9b17f49e662b83d68aa9b166ceae8
jadilovic <adilovic79@yahoo.com>
2020-10-20 17:49:33 +0200

    time interval

--- src/main/resources/static/js/interval.js
+++ src/main/resources/static/js/interval.js
@@ -7,11 +7,11 @@ const inputName = document.getElementById("name");
 const btnGreet = document.querySelector(".greet");
 const btnCancel = document.querySelector(".cancel");
 
-btnCancel.disable = true;
+btnCancel.disabled = true;
 
 function sayHi(who){
-	alert("Welcom dear " + who + "!");
-	btnCancel.disable = true;
+	alert("Welcome dear " + who + "!");
+	btnCancel.disabled = true;
 }
 
 let myGreeting;
@@ -19,19 +19,39 @@ let myGreeting;
 inputForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 	
-	btnCancel.disable = false;
+	btnCancel.disabled = false;
 	
 	myGreeting = setTimeout(sayHi, 4000, inputName.value);
 });
 
-btnCancel.addEventListener("click", () = >{
+btnCancel.addEventListener("click", () =>{
 	clearTimeout(myGreeting);
-	btnCancel.disable = true;
+	btnCancel.disabled = true;
 	console.log("Greeting canceled!");
 })
 
 
+// Wathching time
 
+function displayTime(){
+	let date = new Date();
+	let time = date.toLocaleTimeString();
+	document.getElementById("demo").textContent = time;
+}
+
+const createClock = setInterval(displayTime, 1000);
+
+function displayClock(){
+	let date = new Date();
+	let time = date.toLocaleTimeString();
+	document.querySelector(".clock").textContent = time;
+}
+
+displayClock();
+const createTimeClock = setInterval(displayClock, 1000);
+
+
+clearInterval(createClock);
 
 
 
