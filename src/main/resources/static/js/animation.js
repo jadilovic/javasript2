18eea5e412d97479d8895a156be8ad348c3bf6cf
jadilovic <adilovic79@yahoo.com>
2020-10-23 14:02:50 +0200

    promises images

--- src/main/resources/static/js/animation.js
+++ src/main/resources/static/js/animation.js
@@ -119,6 +119,72 @@ function setEndgame(){
 }
 
 
+// Promise
+
+let promise = fetch("images/coffee.jpg");
+
+let promise2 = promise.then(response => {
+	if(!response.ok){
+		throw new Error(`HTTP Error! status: ${response.status}`);
+	} else {
+		return response.blob();
+	}
+});
+
+let promise3 = promise2.then(myBlob => {
+	let objectURL = URL.createObjectURL(myBlob);
+	let image = document.createElement("img");
+	image.src = objectURL;
+	document.body.appendChild(image);
+});
+
+let errorCase = promise3.catch(e => {
+	console.log("There has been problem with your fetch operation " + e.message);
+});
+
+// short version
+
+fetch("images/oko.jpg")
+.then(response => {
+	if(!response.ok){
+		throw new Error(`HTTP error! status: ${response.status}`);
+	} else {
+		return response.blob();
+	}
+})
+.then(myBlob => {
+	let objectURL = URL.createObjectURL(myBlob);
+	let image = document.createElement("img");
+	image.src = objectURL;
+	document.body.appendChild(image);
+})
+.catch(e => {
+	console.log("There was a problem with your fetch operation " + e.message);
+});
+
+
+// Fetch and Decode
+
+function fetchAndDecode(url, type){
+	return fetch(url).then(response => {
+		if(!response.ok){
+			throw new Error(`HTML Error status: ${response.status}`);
+		} else {
+			if(type === "blob"){
+				return response.blob();
+			} else if (type === "text"){
+				return response.text();
+			}
+		}
+	})
+	.catch(e => {
+		console.log("There was a problem with your catch operation " + e.message);
+	});
+}
+
+
+
+
 
 
 
