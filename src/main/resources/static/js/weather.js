bb9e149634c9a7b4e450681e0a9e0d44c949e38a
jadilovic <adilovic79@yahoo.com>
2021-03-05 23:06:31 +0100

    weather finished

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -8,7 +8,19 @@ window.addEventListener("load", ()=>{
 	
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(position => {
-			console.log(position);
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+			
+			const proxy = "https://cors-anywhere.herokuapp.com/"
+				const api = `${proxy}http://api.weatherapi.com/v1/forecast.xml?key=a1e24f88992a42a882b185946210503&q=${lat}/${long}&days=1&aqi=no&alerts=no`;
+			
+				fetch(api)
+					.then(response =>{
+						return response.json();
+					})
+					.then(data =>{
+					console.log(data);
+				});
 		});
 	}
 });
